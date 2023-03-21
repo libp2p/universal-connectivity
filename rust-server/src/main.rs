159836be1144f366549b4c07131ad19f43522506
Prithvi Shahi <shahi.prithvi@gmail.com>
2023-03-21 12:33:03 -0700

    ValidationMode permissive

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -75,7 +75,7 @@ fn create_swarm<T: RecordStore + std::marker::Send + 'static>() -> Result<Swarm<
     // Set a custom gossipsub configuration
     let gossipsub_config = gossipsub::ConfigBuilder::default()
         .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
-        .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
+        .validation_mode(gossipsub::ValidationMode::Permissive) // This sets the kind of message validation. The default is Strict (enforce message signing)
         .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
         .build()
         .expect("Valid config");
