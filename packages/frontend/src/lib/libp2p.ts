1e58952fb052a00a98a99be54303bfd5e69b4be2
Prithvi Shahi <50885601+p-shahi@users.noreply.github.com>
2023-04-11 19:44:38 -0700

    feat: bootstrap to the ec2 running instance of the rust peer (#40)

--- packages/frontend/src/lib/libp2p.ts
+++ packages/frontend/src/lib/libp2p.ts
@@ -59,7 +59,7 @@ export async function startLibp2p(options: {} = {}) {
     peerDiscovery: [
       bootstrap({
         list: [
-         '/ip4/127.0.0.1/udp/9090/webrtc-direct/certhash/uEiBy_U1UNQ0IDvot_PKlQM_QeU3yx-zCAVaMxxVm2JxWBg/p2p/12D3KooWGTDZj1zAjMCJ8XXx9Z88zAAd6vn3krQYLwZ67S4vMUxz',
+         '/ip4/18.195.246.16/udp/9090/webrtc-direct/certhash/uEiBy_U1UNQ0IDvot_PKlQM_QeU3yx-zCAVaMxxVm2JxWBg/p2p/12D3KooWGTDZj1zAjMCJ8XXx9Z88zAAd6vn3krQYLwZ67S4vMUxz',
         ],
       }),
     ],
