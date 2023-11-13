51dedf69c4d69a2b436448aed7e6ed4e2f0e35bc
Daniel N <2color@users.noreply.github.com>
2023-11-13 11:21:18 +0300

    change default boostrap node

--- js-peer/src/lib/constants.ts
+++ js-peer/src/lib/constants.ts
@@ -5,4 +5,4 @@ export const FILE_EXCHANGE_PROTOCOL = "/universal-connectivity-file/1"
 export const CIRCUIT_RELAY_CODE = 290
 
 export const WEBRTC_BOOTSTRAP_NODE = "/ip4/18.195.246.16/udp/9090/webrtc-direct/certhash/uEiA8EDMfADmULSe2Bm1vVDSmN2RQPvY5MXkEZVOSyD1y2w/p2p/12D3KooWSmtsbL2ukwVwf8gDoTYZHnCd7sVNNVdMnCa4MkWjLujm"
-export const WEBTRANSPORT_BOOTSTRAP_NODE = "/ip4/3.125.128.80/udp/9095/quic-v1/webtransport/certhash/uEiAGIlVdiajNz0k1RHjrxlNXN5bb7W4dLPvMJYUrGJ9ZUQ/certhash/uEiDYZsZoO8vuTKlPhxvVR5SFwOkbXfjlsmTLUHNlnG24bg/p2p/12D3KooWEymoJRHaxizLrrKgJ9MhEYpG85fQ7HReRMJuEMLqmNMg"
+export const WEBTRANSPORT_BOOTSTRAP_NODE = "/ip4/164.92.229.178/udp/9095/quic-v1/webtransport/certhash/uEiDZIdF6aYjPFG_hNYUdJvnsXrlrb6SYbfih3YF239Eujw/certhash/uEiD9iIyO-ZuQeit4oxL5WwppcToPHM9nFmyZOYjRf2Z6ww/p2p/12D3KooWPi5w2xUemmikdhjGH6fvXVM7ZRb59rwRQmUyesJK3wYy"
