241870335f6ad71914c76895f330d27956ddacde
dozyio <github@dozy.io>
2023-12-06 00:26:46 +0000

    chore: connection errors and spinner

--- js-peer/src/pages/index.tsx
+++ js-peer/src/pages/index.tsx
@@ -15,6 +15,8 @@ export default function Home() {
   const { peerStats, setPeerStats } = usePeerContext()
   const { listenAddresses, setListenAddresses } = useListenAddressesContext()
   const [maddr, setMultiaddr] = useState('')
+  const [dialing, setDialing] = useState(false)
+  const [err, setErr] = useState('')
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -78,17 +80,26 @@ export default function Home() {
 
   const handleConnectToMultiaddr = useCallback(
     async (e: React.MouseEvent<HTMLButtonElement>) => {
+      setErr('')
+
       if (!maddr) {
         return
       }
+
+      setDialing(true)
 
       try {
         const connection = await connectToMultiaddr(libp2p)(multiaddr(maddr))
         console.log('connection: ', connection)
 
         return connection
-      } catch (e) {
+      } catch (e: any) {
+        if (e && e.message) {
+          setErr(e.message)
+        }
         console.error(e)
+      } finally {
+        setDialing(false)
       }
     },
     [libp2p, maddr],
@@ -163,11 +174,24 @@ export default function Home() {
                 </div>
                 <button
                   type="button"
-                  className="rounded-md bg-indigo-600 my-2 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className={"rounded-md bg-indigo-600 my-2 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" + (dialing ? ' cursor-not-allowed' : '')}
                   onClick={handleConnectToMultiaddr}
+                  disabled={dialing}
                 >
+                  {dialing && (
+                    <svg className="inline-block text-gray-300 animate-spin" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg"
+                    width="20" height="20">
+                    <path
+                      d="M32 3C35.8083 3 39.5794 3.75011 43.0978 5.20749C46.6163 6.66488 49.8132 8.80101 52.5061 11.4939C55.199 14.1868 57.3351 17.3837 58.7925 20.9022C60.2499 24.4206 61 28.1917 61 32C61 35.8083 60.2499 39.5794 58.7925 43.0978C57.3351 46.6163 55.199 49.8132 52.5061 52.5061C49.8132 55.199 46.6163 57.3351 43.0978 58.7925C39.5794 60.2499 35.8083 61 32 61C28.1917 61 24.4206 60.2499 20.9022 58.7925C17.3837 57.3351 14.1868 55.199 11.4939 52.5061C8.801 49.8132 6.66487 46.6163 5.20749 43.0978C3.7501 39.5794 3 35.8083 3 32C3 28.1917 3.75011 24.4206 5.2075 20.9022C6.66489 17.3837 8.80101 14.1868 11.4939 11.4939C14.1868 8.80099 17.3838 6.66487 20.9022 5.20749C24.4206 3.7501 28.1917 3 32 3L32 3Z"
+                      stroke="white" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path
+                      d="M32 3C36.5778 3 41.0906 4.08374 45.1692 6.16256C49.2477 8.24138 52.7762 11.2562 55.466 14.9605C58.1558 18.6647 59.9304 22.9531 60.6448 27.4748C61.3591 31.9965 60.9928 36.6232 59.5759 40.9762"
+                      stroke="currentColor" stroke-width="5" stroke-linecap="round" stroke-linejoin="round" className="text-indigo-500">
+                    </path>
+                  </svg>)}{' '}
                   Connect to multiaddr
                 </button>
+                {err && <p className="text-red-500">{err}</p>}
               </div>
 
               <div className="my-4 inline-flex items-center text-xl">
