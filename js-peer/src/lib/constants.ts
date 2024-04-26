3b4dce3e3c97bf9f17f735365dad917e7ee4de48
Daniel N <2color@users.noreply.github.com>
2024-04-26 12:45:08 +0200

    feat: delegated routing for bootstrap multiaddrs

--- js-peer/src/lib/constants.ts
+++ js-peer/src/lib/constants.ts
@@ -4,5 +4,9 @@ export const FILE_EXCHANGE_PROTOCOL = "/universal-connectivity-file/1"
 
 export const CIRCUIT_RELAY_CODE = 290
 
-export const WEBRTC_BOOTSTRAP_NODE = "/ip4/147.28.186.157/udp/9090/webrtc-direct/certhash/uEiBbC9bbdvraVWDvcvCEdJAWDymmUqiJQ964FuyEq0hELw/p2p/12D3KooWGahRw3ZnM4gAyd9FK75v4Bp5keFYTvkcAwhpEm28wbV3"
-export const WEBTRANSPORT_BOOTSTRAP_NODE = "/ip4/147.28.186.157/udp/9095/quic-v1/webtransport/certhash/uEiCmLPMgXJ1F1wQ-OgOWJEVa_SYB_jLSf5IkQ_d3V98GBQ/certhash/uEiB-ti6URtr64LV8HYDMvZzzzrb1iNEIT-vGY0yd6UYk2g/p2p/12D3KooWFhXabKDwALpzqMbto94sB7rvmZ6M28hs9Y9xSopDKwQr"
+// 👇 The multiaddrs below are ephemeral and not recommended for use as they expire after a couple of weeks. Instead PeerIDs with peer routing is used
+// export const WEBRTC_BOOTSTRAP_MULTIADDR = "/ip4/147.28.186.157/udp/9090/webrtc-direct/certhash/uEiBbC9bbdvraVWDvcvCEdJAWDymmUqiJQ964FuyEq0hELw/p2p/12D3KooWGahRw3ZnM4gAyd9FK75v4Bp5keFYTvkcAwhpEm28wbV3"
+// export const WEBTRANSPORT_BOOTSTRAP_MULTIADDR = "/ip4/147.28.186.157/udp/9095/quic-v1/webtransport/certhash/uEiCmLPMgXJ1F1wQ-OgOWJEVa_SYB_jLSf5IkQ_d3V98GBQ/certhash/uEiB-ti6URtr64LV8HYDMvZzzzrb1iNEIT-vGY0yd6UYk2g/p2p/12D3KooWFhXabKDwALpzqMbto94sB7rvmZ6M28hs9Y9xSopDKwQr"
+
+export const WEBRTC_BOOTSTRAP_PEER_ID = "12D3KooWGahRw3ZnM4gAyd9FK75v4Bp5keFYTvkcAwhpEm28wbV3"
+export const WEBTRANSPORT_BOOTSTRAP_PEER_ID = "12D3KooWFhXabKDwALpzqMbto94sB7rvmZ6M28hs9Y9xSopDKwQr"
--- js-peer/src/lib/libp2p.ts
+++ js-peer/src/lib/libp2p.ts
@@ -1,42 +1,46 @@
+import { IDBDatastore } from 'datastore-idb'
+import { createDelegatedRoutingV1HttpApiClient } from '@helia/delegated-routing-v1-http-api-client'
 import { createLibp2p, Libp2p } from 'libp2p'
 import { identify } from '@libp2p/identify'
+import {peerIdFromString} from '@libp2p/peer-id'
 import { noise } from '@chainsafe/libp2p-noise'
 import { yamux } from '@chainsafe/libp2p-yamux'
 import { bootstrap } from '@libp2p/bootstrap'
-import { kadDHT } from '@libp2p/kad-dht'
-import {
-  Multiaddr,
-} from '@multiformats/multiaddr'
+import { Multiaddr } from '@multiformats/multiaddr'
 import { sha256 } from 'multiformats/hashes/sha2'
 import type { Message, SignedMessage } from '@libp2p/interface'
 import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { webSockets } from '@libp2p/websockets'
 import { webTransport } from '@libp2p/webtransport'
 import { webRTC, webRTCDirect } from '@libp2p/webrtc'
-import { CHAT_FILE_TOPIC, CHAT_TOPIC, WEBRTC_BOOTSTRAP_NODE, WEBTRANSPORT_BOOTSTRAP_NODE } from './constants'
+import { CHAT_FILE_TOPIC, CHAT_TOPIC, WEBRTC_BOOTSTRAP_PEER_ID, WEBTRANSPORT_BOOTSTRAP_PEER_ID } from './constants'
 import * as filters from "@libp2p/websockets/filters"
 import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
 
 export async function startLibp2p() {
   // enable verbose logging in browser console to view debug logs
-  // localStorage.debug = 'libp2p*,-*:trace'
+  localStorage.debug = 'libp2p*,-*:trace'
 
   // application-specific data lives in the datastore
+  const datastore = new IDBDatastore('universal-connectivity')
+
+  await datastore.open()
 
   const libp2p = await createLibp2p({
+    // datastore,
     addresses: {
       listen: [
+        // 👇 Listen for webRTC connection
         '/webrtc'
       ]
     },
     transports: [
       webTransport(),
-      webSockets({
-        filter: filters.all,
-      }),
+      webSockets(),
       webRTC({
         rtcConfiguration: {
           iceServers: [{
+            // STUN servers help the browser discover its own public IPs
             urls: [
               'stun:stun.l.google.com:19302',
               'stun:global.stun.twilio.com:3478'
@@ -45,42 +49,57 @@ export async function startLibp2p() {
         }
       }),
       webRTCDirect(),
+      // 👇 Required to create circuit relay reservations in order to hole punch browser-to-browser WebRTC connections
       circuitRelayTransport({
         discoverRelays: 1,
       })
     ],
     connectionManager: {
       maxConnections: 10,
-      minConnections: 5
+      minConnections: 3
     },
     connectionEncryption: [noise()],
     streamMuxers: [yamux()],
     connectionGater: {
       denyDialMultiaddr: async () => false,
     },
-    peerDiscovery: [
-      bootstrap({
-        list: [
-          WEBRTC_BOOTSTRAP_NODE,
-          WEBTRANSPORT_BOOTSTRAP_NODE,
-        ],
-      }),
-    ],
+    // The app-specific go and rust peers use WebTransport and WebRTC-direct which have ephemeral multiadrrs that change.
+    // Thus, we dial them using only their peer id below, with delegated routing to discovery their multiaddrs
+    // peerDiscovery: [
+      // bootstrap({
+        // list: [
+          // '12D3KooWFhXabKDwALpzqMbto94sB7rvmZ6M28hs9Y9xSopDKwQr'
+          // WEBRTC_BOOTSTRAP_NODE,
+          // WEBTRANSPORT_BOOTSTRAP_NODE,
+          // '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+          // '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
+          // '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+          // '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+        // ],
+      // }),
+    // ],
     services: {
       pubsub: gossipsub({
         allowPublishToZeroTopicPeers: true,
         msgIdFn: msgIdFnStrictNoSign,
         ignoreDuplicatePublishError: true,
       }),
-      dht: kadDHT({
-        clientMode: true,
-      }),
+      // Delegated routing helps us discover the ephemeral multiaddrs of the dedicated go and rust bootstrap peers
+      // This relies on the public delegated routing endpoint https://docs.ipfs.tech/concepts/public-utilities/#delegated-routing
+      delegatedRouting: () => createDelegatedRoutingV1HttpApiClient('https://delegated-ipfs.dev'),
       identify: identify()
     },
   })
 
   libp2p.services.pubsub.subscribe(CHAT_TOPIC)
   libp2p.services.pubsub.subscribe(CHAT_FILE_TOPIC)
+
+  // Try connecting to bootstrap ppers
+  Promise.all([
+    libp2p.dial(peerIdFromString(WEBRTC_BOOTSTRAP_PEER_ID)),
+    libp2p.dial(peerIdFromString(WEBTRANSPORT_BOOTSTRAP_PEER_ID))
+  ])
+  .catch(e => {console.log('woot', e)})
 
   libp2p.addEventListener('self:peer:update', ({ detail: { peer } }) => {
     const multiaddrs = peer.addresses.map(({ multiaddr }) => multiaddr)
