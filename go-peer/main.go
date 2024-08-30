fb4e60bfc4ee82215fbf53b78dcecb060a99b208
Daniel Norman <1992255+2color@users.noreply.github.com>
2024-08-30 16:19:59 +0200

    Apply suggestions from code review

--- go-peer/main.go
+++ go-peer/main.go
@@ -172,8 +172,7 @@ func main() {
 		libp2p.Transport(webtransport.New),
 		libp2p.Transport(webrtc.New),
 		libp2p.ListenAddrStrings("/ip4/0.0.0.0/udp/9095/quic-v1", "/ip4/0.0.0.0/udp/9095/quic-v1/webtransport"),
-		// 👇 webrtc-direct cannot listen on the same port as QUIC or WebTransport
-		libp2p.ListenAddrStrings("/ip4/0.0.0.0/udp/9096/webrtc-direct"),
+		libp2p.ListenAddrStrings("/ip4/0.0.0.0/udp/9095/webrtc-direct"),
 		libp2p.ListenAddrStrings("/ip6/::/udp/9095/quic-v1", "/ip6/::/udp/9095/quic-v1/webtransport"),
 	)
 
