5ea34ee58adc0f8745bb22956c7544c8e619af4e
Dave Grantham <dwg@linuxprogrammer.org>
2025-04-02 18:23:08 -0600

    fix clippy
    Signed-off-by: Dave Grantham <dwg@linuxprogrammer.org>

--- rust-peer/src/ui/mod.rs
+++ rust-peer/src/ui/mod.rs
@@ -1,6 +1,10 @@
 /// the async UI trait
-pub mod ui;
-pub use ui::Ui;
+/// the async UI trait
+#[async_trait::async_trait]
+pub trait Ui: Send {
+    /// Run the UI
+    async fn run(&mut self) -> anyhow::Result<()>;
+}
 
 /// the TUI implementation
 pub mod tui;
--- rust-peer/src/ui/ui.rs
+++
@@ -1,6 +0,0 @@
-/// the async UI trait
-#[async_trait::async_trait]
-pub trait Ui: Send {
-    /// Run the UI
-    async fn run(&mut self) -> anyhow::Result<()>;
-}
